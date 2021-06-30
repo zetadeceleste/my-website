4c293738a204c7c873b6182c8eb195c30baf3732
czapata <czapata@aconcaguasf.com>
2021-06-30 13:55:52 -0300

    WIP: website / Update styles / Change Index for Home

--- src/App.js
+++ src/App.js
@@ -1,12 +1,24 @@
-import './App.css';
+import React from 'react';
+import './styles/main.scss';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+
+import Home from './pages/Home';
 
 const App = () => {
+   const { PUBLIC_URL } = process.env;
    return (
-      <div className="App">
-         <header className="App-header">
-            <p>Cele's website</p>
-         </header>
-      </div>
+      <BrowserRouter basename={PUBLIC_URL}>
+         <div className="layout">
+            <Switch>
+               <Route exact path="/" component={Home} />
+               {/* <Route path="/about" component={About} /> */}
+               {/* <Route path="/projects" component={Projects} /> */}
+               {/* <Route path="/contact" component={Contact} /> */}
+               {/* <Route path="/resume" component={Resume} /> */}
+               {/* <Route component={NotFound} status={404} /> */}
+            </Switch>
+         </div>
+      </BrowserRouter>
    );
 };
 
